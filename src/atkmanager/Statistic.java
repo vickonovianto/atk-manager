57330445c1fb01616b7b69eea9ba04b60bff3745
bayuRP <13513046@std.stei.itb.ac.id>
2016-02-09 13:51:47 +0700

    minor fixes

--- src/atkmanager/Statistic.java
+++ src/atkmanager/Statistic.java
@@ -1,9 +1,15 @@
 package atkmanager;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
 /**
  *
  * @author adinb
  */
 public class Statistic {
+    ArrayList <Integer> numbers;
+    
+    //String sql = "SELECT SUM(jumlah) FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '" + year + "-" + month + "';";
     
 }
