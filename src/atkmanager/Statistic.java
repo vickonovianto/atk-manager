c4146b2906305fa4a3a146778251559d89ba659c
bayuRP <13513046@std.stei.itb.ac.id>
2016-02-09 23:33:11 +0700

    yearlyStatistics added

--- src/atkmanager/Statistic.java
+++ src/atkmanager/Statistic.java
@@ -1,15 +1,36 @@
 package atkmanager;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author adinb
  */
 public class Statistic {
+    DBHelper db = new DBHelper();
     ArrayList <Integer> numbers;
     
-    //String sql = "SELECT SUM(jumlah) FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '" + year + "-" + month + "';";
+    public ArrayList <Integer> yearlyUsageStatistics(int year, ATK item) {
+        ArrayList <Integer> yearlyStatistics = new ArrayList(12);
+        String sql;
+        for (int month = 1; month <= 12; month++) {
+            sql = "SELECT SUM(jumlah) as total FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '0" + year + "-0" + month + "' AND id_atk = " + item.getID() + ";";
+            ResultSet rs = db.runQuery(sql);
+            try {
+                if(rs.next()) {
+                    yearlyStatistics.add(rs.getInt("total"));
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(Statistic.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return yearlyStatistics;
+    }
+    
     
 }
