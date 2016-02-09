520761ab2b2a366b1f88a47923eb9f7ce42f2cbd
icalF <laser.survivor@gmail.com>
2016-02-09 18:16:43 +0700

    Revert "minor fixes"
    This reverts commit 57330445c1fb01616b7b69eea9ba04b60bff3745.

--- src/atkmanager/Statistic.java
+++ src/atkmanager/Statistic.java
@@ -1,15 +1,9 @@
 package atkmanager;
 
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-
 /**
  *
  * @author adinb
  */
 public class Statistic {
-    ArrayList <Integer> numbers;
-    
-    //String sql = "SELECT SUM(jumlah) FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '" + year + "-" + month + "';";
     
 }
