ef9733914a12936830a2b80841223baf735a1491
bayuRP <13513046@std.stei.itb.ac.id>
2016-02-10 15:27:11 +0700

    More statistics added, renamed some names

--- src/atkmanager/Statistic.java
+++ src/atkmanager/Statistic.java
@@ -12,24 +12,45 @@ import java.util.logging.Logger;
  * @author adinb
  */
 public class Statistic {
-    DBHelper db = new DBHelper();
+    DBHelper db;
     ArrayList <Integer> numbers;
+    ArrayList <Integer> ids;
     
-    public ArrayList <Integer> yearlyUsageStatistics(int year, ATK item) {
-        ArrayList <Integer> yearlyStatistics = new ArrayList(12);
+    public Statistic() {
+        db = new DBHelper();
+    }
+    
+    public void itemUsageStatistics(int year, ATK item) {
+        //ArrayList <Integer> usageStatistics = null;//new ArrayList(12);
+        numbers = new ArrayList();
         String sql;
         for (int month = 1; month <= 12; month++) {
             sql = "SELECT SUM(jumlah) as total FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '0" + year + "-0" + month + "' AND id_atk = " + item.getID() + ";";
             ResultSet rs = db.runQuery(sql);
             try {
                 if(rs.next()) {
-                    yearlyStatistics.add(rs.getInt("total"));
+                    numbers.add(rs.getInt("total"));
                 }
             } catch (SQLException ex) {
                 Logger.getLogger(Statistic.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
-        return yearlyStatistics;
     }
     
+    public void userItemStatistics(int year, int month, User user) {
+        numbers = new ArrayList();
+        ids = new ArrayList();
+        String sql;
+        
+        sql = "SELECT DISTINCT id_atk, SUM(jumlah) as total FROM pesananATK WHERE strftime('%Y-%m', tanggal_pesanan) = '0" + year + "-0" + month + "' AND id_pemesan = " + user.getID() + ";";
+        ResultSet rs = db.runQuery(sql);
+        try {
+            while(rs.next()) {
+                numbers.add(rs.getInt("total"));
+                ids.add(rs.getInt("id_atk"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Statistic.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
